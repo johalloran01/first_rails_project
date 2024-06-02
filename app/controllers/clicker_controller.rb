2d7aee7a1dffc54f4e1e09c7fb4f3f921cac8aa7
johalloran01 <joshuamohalloran@gmail.com>
2024-06-02 18:04:13 -0400

    Working with gas
    Added some logic for total_count and updated view

--- app/controllers/clicker_controller.rb
+++ app/controllers/clicker_controller.rb
@@ -1,10 +1,11 @@
 class ClickerController < ApplicationController
   def index
     @pastries = Pastry.all
+    @total_count = @pastries.sum{ |pastry| pastry.clicks}
   end
 
   def click
-    @pastry = Pastry.find([:id])
+    @pastry = Pastry.find(params[:id])
     @pastry = Pastry.increment(clicks)
 
     if @pastry.clicks >= 100
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post 'clickers/click'
-  get 'clickers/show'
+  get 'clickers/', to:"clickers#index"
   post 'clickers/clear'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "clickers#show"
+  root "clickers#index"
 end
