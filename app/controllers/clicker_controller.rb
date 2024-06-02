7dc04ed5d0263d4a1bc71cc02577b4a1cd0600c5
johalloran01 <joshuamohalloran@gmail.com>
2024-06-02 12:01:39 -0400

    Got rid of the clear function and updated Show

--- app/controllers/clicker_controller.rb
+++ app/controllers/clicker_controller.rb
@@ -17,21 +17,14 @@ class ClickerController < ApplicationController
 
   private
   def bake_pastry
-
     img, name = AssistantBakerHelper.random_name_and_image
     Pastry.create(name: name, img: img, clicks: 0)
   end
 
 
   def show
-    @clicker = Clicker.first
+    @pastry = Pastry.first
   end
 
-  def clear
-    @clicker = Clicker.first
-    @clicker.clicks = 0
-    @clicker.save!
-    redirect_to pastries_show_path
-  end
 
 end
