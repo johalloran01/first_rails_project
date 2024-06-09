3017dde613da0b9284021a1de6106d74bac61ded
johalloran01 <joshuamohalloran@gmail.com>
2024-06-09 13:06:23 -0400

    Minor Modifications to the CSS
    Just looking to fit more onto the page on load.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import anime from 'animejs'; // note that this is only available in the scope of this file, this is how ES6 modularlizes
-                             // JS code.
+// JS code.
 
 // window.anime = anime // This is a hack you can use to add it to the global scope, but it's kind of discouraged except
-                        // for cases where you truly need to have a library in the global scope
+// for cases where you truly need to have a library in the global scope
